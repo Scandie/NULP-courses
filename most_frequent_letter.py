1ce4191d6b2f20bf8119f276329eab17c33b11df
Scandie <grizly.vl@gmail.com>
2015-11-30 17:55:05 +0200

    Delete most_frequent_letter.py

--- most_frequent_letter.py
+++
@@ -1,9 +0,0 @@
-def most_frequent_letter(string):
-    string = string.lower()
-    string = filter(str.isalnum, string)
-    string = sorted(string)
-    most_frequent = string[0]
-    for letter in string[1:]:
-        if string.count(letter) > string.count(most_frequent):
-            most_frequent = letter
-    return most_frequent
