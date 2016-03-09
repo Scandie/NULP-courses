1bd5ec78812d40ff53343fb2e9c0936e3e83de28
Scandie <grizly.vl@gmail.com>
2016-03-09 02:10:12 +0200

    added Task2 solution

--- HM4/decorators.py
+++ HM4/decorators.py
@@ -1,4 +1,5 @@
 from functools import wraps
+import time
 
 # === Task 1 ===
 """
@@ -29,3 +30,77 @@ def multiply(x, y):
 
 print multiply(2, 3)
 print multiply(y=5, x=2)
+
+# === Task 2 ===
+"""
+
+Write a decorator to memoize functions with an arbitrary set of arguments.
+Memoization is only possible if the arguments are hashable. Example function - Fibonacci.
+
+Memoization - if function was already called with this arguments, then get result from cache. if not - execute function,
+ store result in cache, and return result.
+
+If the wrapper is called with arguments which are not hashable, then the wrapped function should just be called without
+caching.
+
+Note: To use args and kwargs as dictionary keys, they must be hashable, which basically means that they must be
+immutable. args is already a tuple, which is fine, but kwargs have to be converted.
+One way is tuple(sorted(kwargs.items())).
+
+Such functions could be used to reduce cost of computation of some functions.
+"""
+
+func_memo = {}
+
+
+def memo(func):   # not sure about proper work of this wrapper
+
+    @wraps(func)
+    def memo_wrapper(*args, **kwargs):
+        t = time.time()
+        if kwargs and not args:
+
+            n = (sorted(kwargs.items())[0][1])  # to simplify
+
+            if n <= 1 or n == 2:
+                return 1
+
+            if n not in func_memo:               # actual wrapping work part
+                func_memo[n] = func(**kwargs)
+                print 'time without wasted %f' % (time.time() - t)    # use to show a point of using memoize
+                return func(**kwargs)
+
+            # print func_memo
+            print 'time with wasted %f' % (time.time() - t)
+            return func_memo[n]
+
+        elif args and not kwargs:
+            n = args[0]                         # to simplify
+            if n <= 1 or n == 2:
+                return 1
+
+            if n not in func_memo:              # actual wrapping work part
+                func_memo[n] = func(*args)
+                print 'time wasted %f' % (time.time() - t)
+                return func(*args)
+
+            # print func_memo
+            print 'time wasted %f' % (time.time() - t)
+            return func_memo[n]
+
+    return memo_wrapper
+
+
+@memo
+def fib(n):
+
+    if n <= 1 or n == 2:
+        return 1
+
+    return int(fib(n=n-1) + fib(n-2))    # 'n=n' for kwargs filling and 'n' for args ones
+
+
+print fib(n=12)
+print func_memo
+print fib(12)
+print func_memo
